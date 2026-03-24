d2663833e67751f9d3790adb7cf110af9726133c
cyber-sapoperro <reclusive_coeditor@proton.me>
2026-03-24 11:28:35 -0500

    Modified the logic to generate graph directly from zk

--- graph_view/cli.py
+++ graph_view/cli.py
@@ -1,38 +1,43 @@
-import argparse
-
 import json
+import subprocess
+import webbrowser
+import tempfile
 from pyvis.network import Network
 
-def make_html_graph(json_path, html_path):
-    with open(json_path) as f:
-        data = json.load(f)
 
+def get_zk_graph():
+    result = subprocess.run(
+        ["zk", "graph", "--format=json"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    data = json.loads(result.stdout)
+    return data
+
+
+def make_html_graph():
+    data = get_zk_graph()
     net = Network(height="100vh", width="100%", directed=True)
-
     # --- Nodes ---
     for note in data["notes"]:
         node_id = note["filenameStem"]  # unique ID
         label = note["title"]
         net.add_node(node_id, label=label)
-
     # --- Edges ---
     for link in data["links"]:
         source = link["sourcePath"].replace(".md", "")
         target = link["targetPath"].replace(".md", "")
         net.add_edge(source, target)
-
+    with tempfile.NamedTemporaryFile(suffix=".html") as f:
+        html_path = f.name
     net.write_html(html_path)
+    return html_path
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i")
-    parser.add_argument("--output", "-o")
-
-    args = parser.parse_args()
-
-    make_html_graph(args.input, args.output)
-
+    html_path = make_html_graph()
+    webbrowser.open(f"file://{html_path}")
 
 if __name__ == "__main__":
     main()
