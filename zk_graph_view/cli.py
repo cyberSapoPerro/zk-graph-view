4d7c2bc9d87c0d7a73835621d570a7835af88b8d
cyber-sapoperro <reclusive_coeditor@proton.me>
2026-03-25 18:15:21 -0500

    Added colors for the tags

--- zk_graph_view/cli.py
+++ zk_graph_view/cli.py
@@ -3,6 +3,7 @@ import subprocess
 import webbrowser
 import tempfile
 from pyvis.network import Network
+import colorir as cl
 
 
 def get_zk_graph():
@@ -19,16 +20,34 @@ def get_zk_graph():
 def make_html_graph():
     data = get_zk_graph()
     net = Network(height="100vh", width="100%", directed=True)
+
+    # --- Tags ---
+    tags = [note["tags"][0] if note["tags"] else "untagged" for note in data["notes"]]
+
+    # -- Color map ---
+    grad = cl.PolarGrad(["ffff00", "ff00ff"])  # Creates a gradient from yellow to magenta
+    palette = cl.StackPalette(grad.n_colors(len(tags)))
+    palette *= cl.HCLab(1, 0.5, 1)  # Desaturates the palette 50% to get a more pleasing look
+    color_map = {tag: color for tag, color in zip(tags, palette)}
+    color_map["untagged"] = cl.Hex("#808080")
+
     # --- Nodes ---
     for note in data["notes"]:
         node_id = note["filenameStem"]  # unique ID
         label = note["title"]
-        net.add_node(node_id, label=label)
+        if note["tags"]:
+            tag = note["tags"][0]
+        else:
+            tag = "untagged"
+        net.add_node(node_id, label=label, color=color_map[tag])
+
     # --- Edges ---
     for link in data["links"]:
         source = link["sourcePath"].replace(".md", "")
         target = link["targetPath"].replace(".md", "")
         net.add_edge(source, target)
+
+    # --- Open the graph ---
     with tempfile.NamedTemporaryFile(suffix=".html") as f:
         html_path = f.name
     net.write_html(html_path)
